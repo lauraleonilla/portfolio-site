6649843dcf992789481e90640040c0b0b3d336bf
Laura Forsblom <lauralforsblom@gmail.com>
2024-04-12 11:09:29 +0300

    Added MUI & jest

---
+++ src/styles/globalStyle.ts
@@ -0,0 +1,10 @@
+import { createGlobalStyle } from 'styled-components'
+
+const GlobalStyles = createGlobalStyle`
+body {
+  font-family: 'Roboto Condensed', sans-serif;
+  padding: 20px;
+}
+`
+
+export default GlobalStyles
