b6a5a2a25b6c87e9441da8ad0ba4e5069189fd1f
lauraleonilla <lauralforsblom@gmail.com>
2025-12-27 23:56:48 +0200

    Fixed filename and lint in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,27 +1,24 @@
-import './index.css'
+import './index.css';
 
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-import App from './App.tsx'
+import App from './App.tsx';
 import About from './pages/About.tsx';
 import Blog from './pages/Blog.tsx';
 
 const router = createBrowserRouter([
   {
-    path: "/",
+    path: '/',
     element: <App />,
   },
   {
-    path: "blog",
+    path: 'blog',
     element: <Blog />,
   },
   {
-    path: "about",
+    path: 'about',
     element: <About />,
   },
 ]);
@@ -29,5 +26,5 @@ const router = createBrowserRouter([
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <RouterProvider router={router} />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
